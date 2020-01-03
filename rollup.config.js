205b088baea79b7d06f528e908458750ed01ce88
Tomas Malbran <malbrantomas@gmail.com>
2020-01-03 01:36:02 -0300

    Better build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import fastGlob         from "fast-glob";
+import path             from "path";
 import svgr             from "@svgr/rollup";
 import commonjs         from "@rollup/plugin-commonjs";
 import url              from "@rollup/plugin-url";
@@ -5,52 +7,60 @@ import resolve          from "@rollup/plugin-node-resolve";
 import babel            from "rollup-plugin-babel";
 import external         from "rollup-plugin-peer-deps-external";
 import cleaner          from "rollup-plugin-cleaner";
-import multiInput       from "rollup-plugin-multi-input";
-
-// import copy             from "rollup-plugin-copy"
 
 
 
-const plugins = [
-    multiInput(),
-    external(),
-    url(),
-    svgr(),
-    babel({ exclude : "node_modules/**" }),
-    resolve(),
-    commonjs({
-        include      : "node_modules/**",
-        namedExports : {
-            "node_modules/react-is/index.js" : [ "ForwardRef", "isValidElementType", "isElement" ],
+export default function makeConfig(commandOptions) {
+    const isDist  = commandOptions["config-dist"];
+    const plugins = [
+        external(),
+        url(),
+        svgr(),
+        babel({ exclude : "node_modules/**" }),
+        resolve(),
+        commonjs({
+            include      : "node_modules/**",
+            namedExports : {
+                "node_modules/react-is/index.js" : [ "ForwardRef", "isValidElementType", "isElement" ],
+            },
+        }),
+    ];
+
+    const exporter = (src, dst) => {
+        const inputs = fastGlob.sync([ `src/${src}` ]).map((filePath) => {
+            const fileName = path.basename(filePath);
+            return [ fileName.replace(/\.[^/.]+$/, ""), filePath ];
+        });
+        
+        return {
+            input  : Object.fromEntries(inputs),
+            output : {
+                dir    : isDist ? `dist/${dst}` : dst,
+                format : "cjs",
+            },
+            plugins,
+        };
+    };
+
+    return [
+        {
+            input  : {
+                index  : "src/index.js",
+            },
+            output : {
+                dir    : "dist",
+                format : "cjs",
+            },
+            plugins : [
+                cleaner({
+                    targets : [ "./dist/", "./Components", "./Core", "./Utils" ],
+                }),
+                ...plugins,
+            ],
         },
-    }),
-];
 
-export default [
-    {
-        input  : {
-            index  : "src/index.js",
-        },
-        output : {
-            dir    : "dist",
-            format : "cjs",
-        },
-        plugins : [
-            cleaner({
-                targets : [ "./dist/" ],
-            }),
-            // copy({
-            //     targets : [{ src : "package.json", dest : "dist" }],
-            // }),
-            ...plugins,
-        ],
-    },
-    {
-        input  : [ "src/**/*.js" ],
-        output : {
-            dir    : "dist",
-            format : "cjs",
-        },
-        plugins,
-    },
-];
+        exporter("Components/**/*.js", "Components"),
+        exporter("Core/*.js", "Core"),
+        exporter("Utils/*.js", "Utils"),
+    ];
+}
