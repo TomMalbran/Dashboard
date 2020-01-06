cc9d8849d3c556bd9abedbbb98f613d2b61e26eb
Tomas Malbran <malbrantomas@gmail.com>
2020-01-06 19:54:32 -0300

    Examples updates

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { Html } from "dashboard";
-import { Download } from "dashboard";
+
+// Components
+import Result from "dashboard/dist/Components/Result";
+import Drawer from "dashboard/dist/Components/Drawer";
+import Html   from "dashboard/dist/Components/Html";
+
 
 
 export default class App extends React.Component {
     render () {
         return <div>
-            <Html>{"<b>Hola</b><br><i>Como estas</i>"}</Html>
-            <Download src="" />
+            <Result open message="Hola" />
+            <Drawer open>
+                <Html>{"<b>Hola</b><br><i>Como estas</i>"}</Html>
+            </Drawer>
         </div>;
     }
 }
