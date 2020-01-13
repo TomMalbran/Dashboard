30c331c66559a7dee9c1297e42216169dbeb35e2
Tomas Malbran <malbrantomas@gmail.com>
2020-01-12 22:58:51 -0300

    The Dashboard main class. Right now it handles the dialog level

---
+++ src/Core/Dashboard.js
@@ -0,0 +1,39 @@
+let dialogLevel = 0;
+
+
+
+/**
+ * Returns true if the current dialog level is at the given one
+ * @param {Number} level
+ * @returns {Boolean}
+ */
+function isDialogAt(level) {
+    return dialogLevel === level;
+}
+
+/**
+ * Increases the Dialog Level and returns it
+ * @returns {Number}
+ */
+function opnDialog() {
+    dialogLevel += 1;
+    return dialogLevel;
+}
+
+/**
+ * Decreases the Dialog Level and returns 0
+ * @returns {Number}
+ */
+function closeDialog() {
+    dialogLevel -= 1;
+    return 0;
+}
+
+
+
+// The public API
+export default {
+    isDialogAt,
+    opnDialog,
+    closeDialog,
+};
