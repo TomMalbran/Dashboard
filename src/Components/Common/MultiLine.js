c8688ad90fb9457f50eb06a9e7baa06e71cba2a7
Tomas Malbran <malbrantomas@gmail.com>
2020-05-11 12:23:52 -0300

    Add a MultiLine component to show a text with multiple lines

---
+++ src/Components/Common/MultiLine.js
@@ -0,0 +1,65 @@
+import React                from "react";
+import PropTypes            from "prop-types";
+
+// Variants
+const Variant = {
+    P   : "p",
+    DIV : "div",
+};
+
+
+
+/**
+ * The Multi Line Component
+ * @param {Object} props
+ * @returns {React.ReactElement}
+ */
+function MultiLine(props) {
+    const { variant, className, content, children } = props;
+    const cnt   = String(content || children);
+    const lines = cnt.split("\n");
+    
+    // Nothing to display
+    if (!cnt) {
+        return <React.Fragment />;
+    }
+
+    // Only 1 line
+    if (lines.length < 1) {
+        return <div className={className}>{cnt}</div>;
+    }
+    
+    // Multiple lines
+    switch (variant) {
+    case Variant.P:
+        return <div className={className}>
+            {lines.map((elem, index) => <p key={index}>{elem}</p>)}
+        </div>;
+    default:
+        return <div className={className}>
+            {lines.map((elem, index) => <div key={index}>{elem}</div>)}
+        </div>;
+    }
+}
+
+/**
+ * The Property Types
+ * @typedef {Object} propTypes
+ */
+MultiLine.propTypes = {
+    variant   : PropTypes.string,
+    className : PropTypes.string,
+    content   : PropTypes.string,
+    children  : PropTypes.string,
+};
+
+/**
+ * The Default Properties
+ * @type {Object} defaultProps
+ */
+MultiLine.defaultProps = {
+    variant   : Variant.DIV,
+    className : "",
+};
+
+export default MultiLine;
