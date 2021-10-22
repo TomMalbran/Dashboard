1a463b14c54af27c137ff292c50e26913351ee9a
Tomas Malbran <malbrantomas@gmail.com>
2021-10-22 16:31:20 -0300

    Add an option to center the dialog message

--- src/Components/Dialog/DialogMessage.js
+++ src/Components/Dialog/DialogMessage.js
@@ -11,17 +11,19 @@ import Html                 from "../Common/Html";
 
 
 // Styles
-const Div = Styled.div.attrs(({ smallSpace, noSpace }) => ({ smallSpace, noSpace }))`
+const Div = Styled.div.attrs(({ smallSpace, noSpace, centered }) => ({ smallSpace, noSpace, centered }))`
     color: var(--black-color);
     font-weight: 400;
+    ${(props) => props.centered ? "text-align: center;" : ""}
     ${(props) => props.noSpace ? "margin: 0;" : (
         props.smallSpace ? "margin: 0 0 16px 0;" : "margin: 0 0 32px 0;"
     )}
 `;
 
-const Content = Styled(Html).attrs(({ smallSpace, noSpace }) => ({ smallSpace, noSpace }))`
+const Content = Styled(Html).attrs(({ smallSpace, noSpace, centered }) => ({ smallSpace, noSpace, centered }))`
     color: var(--black-color);
     font-weight: 400;
+    ${(props) => props.centered ? "text-align: center;" : ""}
     ${(props) => props.noSpace ? "margin: 0;" : (
         props.smallSpace ? "margin: 0 0 16px 0;" : "margin: 0 0 32px 0;"
     )}
@@ -40,28 +42,33 @@ const Content = Styled(Html).attrs(({ smallSpace, noSpace }) => ({ smallSpace, n
  */
 function DialogMessage(props) {
     const {
-        isHidden, className, variant, smallSpace, noSpace,
+        isHidden, className, variant, smallSpace, noSpace, centered,
         html, message, children,
     } = props;
+
     const content = message ? NLS.get(message) : html;
 
     if (isHidden || (!content && !children)) {
         return <React.Fragment />;
     }
+
     if (children) {
         return <Div
             className={className}
             smallSpace={smallSpace}
             noSpace={noSpace}
+            centered={centered}
         >
             {children}
         </Div>;
     }
+
     return <Content
         variant={variant}
         className={className}
         smallSpace={smallSpace}
         noSpace={noSpace}
+        centered={centered}
     >
         {content}
     </Content>;
@@ -77,6 +84,7 @@ DialogMessage.propTypes = {
     variant    : PropTypes.string,
     smallSpace : PropTypes.bool,
     noSpace    : PropTypes.bool,
+    centered   : PropTypes.bool,
     message    : PropTypes.string,
     html       : PropTypes.string,
     children   : PropTypes.any,
@@ -92,6 +100,7 @@ DialogMessage.defaultProps = {
     variant    : "div",
     smallSpace : false,
     noSpace    : false,
+    centered   : false,
 };
 
 export default DialogMessage;
