7c655f66b116c24b6616c499510cdf9bd77bac07
Tomas Malbran <malbrantomas@gmail.com>
2021-11-19 20:19:57 -0300

    Move the input line height to the select, used for FireFox

--- src/Components/Input/SelectInput.js
+++ src/Components/Input/SelectInput.js
@@ -9,9 +9,9 @@ import NLS                  from "../../Core/NLS";
 
 // Styles
 const Select = Styled.select`
-    text-indent: 0.01px;
     margin: 0;
     padding-right: 24px !important;
+    line-height: 1.7;
     background-color: transparent;
     background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABsAAAAICAYAAAAIloRgAAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwgAADsIBFShKgAAAABp0RVh0U29mdHdhcmUAUGFpbnQuTkVUIHYzLjUuMTAw9HKhAAAAdElEQVQ4T2P4//8/AyGsLK8ggE8NIXmYXoIWgRQCDbuAy0CQOEiekINB8sRa9h+bhTCLgDTQLMIhRKxlIJ+hWIhsEbV9Bg4qJAsd0Ph445SkOIPGG7KFIF9iDVp8wUlUMMIMQA86YlMhyT5Ds3ABqRaB9AMArxAryYUamQYAAAAASUVORK5CYII=);
     background-position: right center;
