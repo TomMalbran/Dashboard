ac81abb9cefe56c826165dc2e9a6892b211b5d24
Tomas Malbran <malbrantomas@gmail.com>
2022-12-03 19:47:42 -0300

    Add a new hook for the Forms

---
+++ src/Utils/Form.js
@@ -0,0 +1,124 @@
+import React                from "react";
+
+
+
+/**
+ * Returns a Hook to use the Form Data and Errors
+ * @param {Object}    initialData
+ * @param {Function}  edit
+ * @param {Function=} onSubmit
+ * @param {Boolean=}  startLoading
+ * @returns {Object}
+ */
+function useForm(initialData, edit, onSubmit = null, startLoading = true) {
+    const initialErrors = { form : "" };
+    for (const key of Object.keys(initialData)) {
+        initialErrors[key] = "";
+    }
+
+    const [ loading, setLoadingInt ] = React.useState(startLoading);
+    const [ data,    setDataInt    ] = React.useState({ ...initialData });
+    const [ errors,  setErrorsInt  ] = React.useState({ ...initialErrors });
+
+
+    // Starts the Loader
+    const startLoader = () => {
+        setLoadingInt(true);
+    };
+
+    // Ends the Loader
+    const endLoader = () => {
+        setLoadingInt(false);
+    };
+
+
+    // Sets the Data
+    const setData = (fields) => {
+        setDataInt({ ...data, ...fields });
+    };
+
+    // Resets the Data
+    const resetData = () => {
+        setDataInt({ ...initialData });
+    };
+
+
+    // Returns the Main Error
+    const mainError = () => {
+        return errors.form;
+    };
+
+    // Sets the Errors
+    const setErrors = (fields) => {
+        setErrorsInt({ ...errors, ...fields });
+    };
+
+    // Resets the Errors
+    const resetErrors = () => {
+        setErrorsInt({ ...initialErrors });
+    };
+
+
+    // Sets the Elem
+    const setElem = (elem) => {
+        setDataInt({ ...initialData, ...elem });
+        setErrorsInt({ ...initialErrors });
+        setLoadingInt(false);
+    };
+
+    // Sets the Position
+    const setPosition = (position) => {
+        setDataInt({ ...initialData, position });
+        setErrorsInt({ ...initialErrors });
+        setLoadingInt(false);
+    };
+
+    // Resets the Data and Errors
+    const reset = () => {
+        resetData();
+        resetErrors();
+        setLoadingInt(false);
+    };
+
+
+    // Handles the Input Change
+    const handleChange = (name, value) => {
+        setData({ [name] : value });
+        setErrors({ [name] : "" });
+    };
+
+    // Starts the Submit
+    const handleSubmit = async () => {
+        if (loading) {
+            return;
+        }
+        startLoader();
+        resetErrors();
+        try {
+            await edit(data);
+            endLoader();
+            if (onSubmit) {
+                onSubmit();
+            }
+        } catch (errors) {
+            endLoader();
+            setErrorsInt(errors);
+        }
+    };
+
+
+    // The API
+    return {
+        loading, startLoader, endLoader,
+        data, setData, resetData,
+        errors, mainError, setErrors, resetErrors,
+        setElem, setPosition, reset,
+        handleChange, handleSubmit,
+    };
+}
+
+
+
+
+// The public API
+export default useForm;
