9d62e9f0fb4faa02376b73232f8ca1ec9c9096df
Tomas Malbran <malbrantomas@gmail.com>
2022-12-05 02:06:09 -0300

    Add a new hook for the Lists

---
+++ src/Utils/List.js
@@ -0,0 +1,64 @@
+import React                from "react";
+
+// Core
+import Store                from "../Core/Store";
+import Navigate             from "../Core/Navigate";
+
+
+
+/**
+ * Returns a Hook to use the List Fetch
+ * @param {String}  slice
+ * @param {String=} type
+ * @returns {Object}
+ */
+function useList(slice, type = "") {
+    const elemID = Navigate.useOneParam(type);
+    const data   = Store.useState(slice);
+    const { startLoader, fetchList } = Store.useAction(slice);
+
+    React.useEffect(() => {
+        load();
+        return () => startLoader();
+    }, []);
+
+    // Fetch the content
+    const fetch = (params = data.sort) => {
+        if (type && data.filter) {
+            fetchList(type, elemID, data.filter, params);
+        } else if (type) {
+            fetchList(type, elemID, params);
+        } else if (data.filter) {
+            fetchList(data.filter, params);
+        } else {
+            fetchList(params);
+        }
+    };
+
+    // Loads the Content
+    const load = (params = data.sort) => {
+        startLoader();
+        fetch(params);
+    };
+
+    // Filters the content after Filter
+    const filter = (filters) => {
+        startLoader();
+        const params = { ...data.sort, page : 0 };
+        if (type) {
+            fetchList(type, elemID, filters, params);
+        } else {
+            fetchList(filters, params);
+        }
+    };
+
+
+
+    return { fetch, load, filter };
+}
+
+
+
+
+// The public API
+export default useList;
