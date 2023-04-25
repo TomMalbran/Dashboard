5e8fba9e265a28bde92e319535b4579caf7be14a
Tomas Malbran <malbrantomas@gmail.com>
2023-04-25 00:18:48 -0300

    Improve the usage and design of the Emoji popup

--- src/Components/Form/EmojiPopup.js
+++ src/Components/Form/EmojiPopup.js
@@ -13,6 +13,12 @@ import EmojiPicker, {
 
 
 
+// Constants
+const POPUP_HEIGHT = 400;
+const TEST_HEIGHT  = 410;
+const POPUP_WIDTH  = 350;
+const TEST_WIDTH   = 360;
+
 // Styles
 const Emoji = Styled.div.attrs(({ top, left }) => ({ top, left }))`
     position: absolute;
@@ -21,15 +27,26 @@ const Emoji = Styled.div.attrs(({ top, left }) => ({ top, left }))`
     z-index: var(--z-menu);
 
     aside {
-        --epr-picker-border-color: var(--lighter-gray);
+        --epr-picker-border-color: white;
         --epr-picker-border-radius: var(--border-radius);
         --epr-category-label-bg-color: var(--lighter-gray);
-        --epr-search-input-bg-color: var(--light-gray);
+        --epr-search-input-bg-color: var(--lighter-gray);
+        --epr-category-label-bg-color: var(--lighter-gray);
+        --epr-category-label-text-color: var(--title-color);
         --epr-emoji-hover-color: var(--dark-gray);
         --epr-search-border-color: var(--primary-color);
         --epr-highlight-color: var(--primary-color);
+        --epr-header-padding: 8px;
+
+        box-sizing: border-box;
         box-shadow: rgb(9 30 66 / 25%) 0px 4px 8px -2px, rgb(9 30 66 / 31%) 0px 0px 1px;
-        background-color: var(--lighter-gray);
+        /* background-color: var(--lighter-gray); */
+    }
+    .EmojiPickerReact ul.epr-emoji-list {
+        padding: 0 8px;
+    }
+    .EmojiPickerReact li.epr-emoji-category>.epr-emoji-category-label {
+        border-radius: var(--border-radius);
     }
 `;
 
@@ -40,9 +57,13 @@ const Emoji = Styled.div.attrs(({ top, left }) => ({ top, left }))`
  * @returns {React.ReactElement}
  */
 function EmojiPopup(props) {
-    const { open, top, left, right, onClick, onClose } = props;
+    const {
+        open, targetRef, direction, gap,
+        top, bottom, left, right,
+        onClick, onClose,
+    } = props;
 
-    const menuRef = React.useRef(null);
+    const contentRef = React.useRef(null);
 
 
     // Handle the Close
@@ -56,34 +77,63 @@ function EmojiPopup(props) {
 
     // Handle the Emoji Close
     const handleClose = (e) => {
-        if (open && !Utils.inRef(e.clientX, e.clientY, menuRef)) {
+        if (open && !Utils.inRef(e.clientX, e.clientY, contentRef)) {
             onClose();
         }
     };
+
+
+    // Return early
+    if (!open) {
+        return <React.Fragment />;
+    }
+
+
+    // Set the position
+    let posTop  = top  || 0;
+    let posLeft = left || 0;
+
+    if (targetRef) {
+        const bounds = Utils.getBounds(targetRef);
+        if (direction.includes("top")) {
+            posTop = bounds.top - gap;
+        } else {
+            posTop = bounds.bottom + gap;
+        }
+        if (posTop && direction.includes("left")) {
+            posLeft = bounds.right - POPUP_WIDTH;
+        } else {
+            posLeft = bounds.left;
+        }
+    } else {
+        if (bottom) {
+            posTop = bottom - POPUP_HEIGHT;
+        }
+        if (right) {
+            posLeft = right - POPUP_WIDTH;
+        }
+    }
+
+    if (posTop + TEST_HEIGHT > window.innerHeight) {
+        posTop -= (posTop + TEST_HEIGHT) - window.innerHeight;
+    }
+    if (posLeft + TEST_WIDTH > window.innerWidth) {
+        posLeft -= (posLeft + TEST_WIDTH) - window.innerWidth;
+    }
 
 
     // Do the Render
     if (!open) {
         return <React.Fragment />;
     }
-
-    const posTop  = Math.max(60, top - 400);
-    let   posLeft = left;
-    if (right) {
-        posLeft = right - 350;
-    }
-    if (posLeft + 360 > window.innerWidth) {
-        posLeft -= (posLeft + 360) - window.innerWidth;
-    }
-
-    return <Emoji ref={menuRef} top={posTop} left={posLeft}>
+    return <Emoji ref={contentRef} top={posTop} left={posLeft}>
         <EmojiPicker
             onEmojiClick={(elem) => onClick(elem.emoji)}
             emojiStyle={EmojiStyle.NATIVE}
             previewConfig={{ showPreview : false }}
             skinTonesDisabled={true}
             searchPlaceHolder={NLS.get("GENERAL_SEARCH")}
-            height={400}
+            height={POPUP_HEIGHT}
             categories={[
                 {
                     category : Categories.SUGGESTED,
@@ -131,12 +181,28 @@ function EmojiPopup(props) {
  * @typedef {Object} propTypes
  */
 EmojiPopup.propTypes = {
-    open    : PropTypes.bool.isRequired,
-    top     : PropTypes.number.isRequired,
-    left    : PropTypes.number,
-    right   : PropTypes.number,
-    onClick : PropTypes.func.isRequired,
-    onClose : PropTypes.func.isRequired,
+    open      : PropTypes.bool,
+    targetRef : PropTypes.any,
+    direction : PropTypes.string,
+    top       : PropTypes.number,
+    bottom    : PropTypes.number,
+    left      : PropTypes.number,
+    right     : PropTypes.number,
+    gap       : PropTypes.number,
+    onClick   : PropTypes.func.isRequired,
+    onClose   : PropTypes.func.isRequired,
+};
+
+/**
+ * The Default Properties
+ * @type {Object} defaultProps
+ */
+EmojiPopup.defaultProps = {
+    open      : false,
+    direction : "",
+    top       : 0,
+    left      : 0,
+    gap       : 0,
 };
 
 export default EmojiPopup;
