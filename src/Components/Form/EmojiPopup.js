4777459f7b504146cbc1cc5d2bbe907317b4658f
Tomas Malbran <malbrantomas@gmail.com>
2023-04-28 18:05:37 -0300

    Some fixes to the Emoji Popup

--- src/Components/Form/EmojiPopup.js
+++ src/Components/Form/EmojiPopup.js
@@ -20,6 +20,14 @@ const POPUP_WIDTH  = 350;
 const TEST_WIDTH   = 360;
 
 // Styles
+const Backdrop = Styled.div`
+    display: block;
+    box-sizing: border-box;
+    position: fixed;
+    inset: 0;
+    z-index: var(--z-menu);
+`;
+
 const Emoji = Styled.div.attrs(({ top, left }) => ({ top, left }))`
     position: absolute;
     top: ${(props) => `${props.top}px`};
@@ -40,7 +48,6 @@ const Emoji = Styled.div.attrs(({ top, left }) => ({ top, left }))`
 
         box-sizing: border-box;
         box-shadow: rgb(9 30 66 / 25%) 0px 4px 8px -2px, rgb(9 30 66 / 31%) 0px 0px 1px;
-        /* background-color: var(--lighter-gray); */
     }
     .EmojiPickerReact ul.epr-emoji-list {
         padding: 0 8px;
@@ -67,19 +74,12 @@ function EmojiPopup(props) {
 
 
     // Handle the Close
-    React.useEffect(() => {
-        if (open) {
-            window.addEventListener("click", handleClose);
-            return () => window.removeEventListener("click", handleClose);
-        }
-        return () => {};
-    }, [ open ]);
-
-    // Handle the Emoji Close
     const handleClose = (e) => {
         if (open && !Utils.inRef(e.clientX, e.clientY, contentRef)) {
             onClose();
         }
+        e.preventDefault();
+        e.stopPropagation();
     };
 
 
@@ -90,17 +90,20 @@ function EmojiPopup(props) {
 
 
     // Set the position
+    const toTop  = direction.includes("top");
+    const toLeft = direction.includes("left");
+
     let posTop  = top  || 0;
     let posLeft = left || 0;
 
     if (targetRef) {
         const bounds = Utils.getBounds(targetRef);
-        if (direction.includes("top")) {
-            posTop = bounds.top - gap;
+        if (toTop) {
+            posTop = bounds.top - POPUP_HEIGHT - gap;
         } else {
             posTop = bounds.bottom + gap;
         }
-        if (posTop && direction.includes("left")) {
+        if (toLeft) {
             posLeft = bounds.right - POPUP_WIDTH;
         } else {
             posLeft = bounds.left;
@@ -114,10 +117,14 @@ function EmojiPopup(props) {
         }
     }
 
-    if (posTop + TEST_HEIGHT > window.innerHeight) {
+    if (toTop && posTop < 0) {
+        posTop = 10;
+    } else if (!toTop && posTop + TEST_HEIGHT > window.innerHeight) {
         posTop -= (posTop + TEST_HEIGHT) - window.innerHeight;
     }
-    if (posLeft + TEST_WIDTH > window.innerWidth) {
+    if (posLeft < 0) {
+        posLeft = 10;
+    } else if (posLeft + TEST_WIDTH > window.innerWidth) {
         posLeft -= (posLeft + TEST_WIDTH) - window.innerWidth;
     }
 
@@ -126,54 +133,56 @@ function EmojiPopup(props) {
     if (!open) {
         return <React.Fragment />;
     }
-    return <Emoji ref={contentRef} top={posTop} left={posLeft}>
-        <EmojiPicker
-            onEmojiClick={(elem) => onClick(elem.emoji)}
-            emojiStyle={EmojiStyle.NATIVE}
-            previewConfig={{ showPreview : false }}
-            skinTonesDisabled={true}
-            searchPlaceHolder={NLS.get("GENERAL_SEARCH")}
-            height={POPUP_HEIGHT}
-            categories={[
-                {
-                    category : Categories.SUGGESTED,
-                    name     : NLS.get("EMOJI_SUGGESTED"),
-                },
-                {
-                    category : Categories.SMILEYS_PEOPLE,
-                    name     : NLS.get("EMOJI_SMILEYS"),
-                },
-                {
-                    category : Categories.ANIMALS_NATURE,
-                    name     : NLS.get("EMOJI_ANIMALS"),
-                },
-                {
-                    category : Categories.FOOD_DRINK,
-                    name     : NLS.get("EMOJI_FOOD"),
-                },
-                {
-                    category : Categories.TRAVEL_PLACES,
-                    name     : NLS.get("EMOJI_TRAVEL"),
-                },
-                {
-                    category : Categories.ACTIVITIES,
-                    name     : NLS.get("EMOJI_ACTIVITIES"),
-                },
-                {
-                    category : Categories.OBJECTS,
-                    name     : NLS.get("EMOJI_OBJECTS"),
-                },
-                {
-                    category : Categories.SYMBOLS,
-                    name     : NLS.get("EMOJI_SYMBOLS"),
-                },
-                {
-                    category : Categories.FLAGS,
-                    name     : NLS.get("EMOJI_FLAGS"),
-                },
-            ]}
-        />
-    </Emoji>;
+    return <Backdrop onClick={handleClose}>
+        <Emoji ref={contentRef} top={posTop} left={posLeft}>
+            <EmojiPicker
+                onEmojiClick={(elem) => onClick(elem.emoji)}
+                emojiStyle={EmojiStyle.NATIVE}
+                previewConfig={{ showPreview : false }}
+                skinTonesDisabled={true}
+                searchPlaceHolder={NLS.get("GENERAL_SEARCH")}
+                height={POPUP_HEIGHT}
+                categories={[
+                    {
+                        category : Categories.SUGGESTED,
+                        name     : NLS.get("EMOJI_SUGGESTED"),
+                    },
+                    {
+                        category : Categories.SMILEYS_PEOPLE,
+                        name     : NLS.get("EMOJI_SMILEYS"),
+                    },
+                    {
+                        category : Categories.ANIMALS_NATURE,
+                        name     : NLS.get("EMOJI_ANIMALS"),
+                    },
+                    {
+                        category : Categories.FOOD_DRINK,
+                        name     : NLS.get("EMOJI_FOOD"),
+                    },
+                    {
+                        category : Categories.TRAVEL_PLACES,
+                        name     : NLS.get("EMOJI_TRAVEL"),
+                    },
+                    {
+                        category : Categories.ACTIVITIES,
+                        name     : NLS.get("EMOJI_ACTIVITIES"),
+                    },
+                    {
+                        category : Categories.OBJECTS,
+                        name     : NLS.get("EMOJI_OBJECTS"),
+                    },
+                    {
+                        category : Categories.SYMBOLS,
+                        name     : NLS.get("EMOJI_SYMBOLS"),
+                    },
+                    {
+                        category : Categories.FLAGS,
+                        name     : NLS.get("EMOJI_FLAGS"),
+                    },
+                ]}
+            />
+        </Emoji>
+    </Backdrop>;
 }
 
 /**
