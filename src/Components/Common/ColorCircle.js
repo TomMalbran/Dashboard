3fddd267bb4e5a699b951ddbb7a15586d65d09b8
Tomas Malbran <malbrantomas@gmail.com>
2023-08-23 15:51:03 -0300

    Add a Color Circle component

---
+++ src/Components/Common/ColorCircle.js
@@ -0,0 +1,42 @@
+import React                from "react";
+import PropTypes            from "prop-types";
+import Styled               from "styled-components";
+
+
+
+// Styles
+const Container = Styled.div.attrs(({ color, size }) => ({ color, size }))`
+    flex-shrink: 0;
+    width: ${(props) => `${props.size}px`};
+    height: ${(props) => `${props.size}px`};
+    background-color: ${(props) => props.color};
+    border-radius: 50%;
+    padding: 0;
+`;
+
+
+
+/**
+ * The Color Circle
+ * @param {Object} props
+ * @returns {React.ReactElement}
+ */
+function ColorCircle(props) {
+    const { color, size } = props;
+
+    return <Container
+        color={color || "white"}
+        size={size || 24}
+    />;
+}
+
+/**
+ * The Property Types
+ * @typedef {Object} propTypes
+ */
+ColorCircle.propTypes = {
+    color : PropTypes.string,
+    size  : PropTypes.oneOfType([ PropTypes.number, PropTypes.string ]),
+};
+
+export default ColorCircle;
