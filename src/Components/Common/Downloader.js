0c5d8c237bb76c6aa63784d0560b3695210efd30
Tomas Malbran <malbrantomas@gmail.com>
2023-08-23 15:51:20 -0300

    Update the on load callback of the Downloader

--- src/Components/Common/Downloader.js
+++ src/Components/Common/Downloader.js
@@ -11,14 +11,37 @@ import PropTypes            from "prop-types";
 function Downloader(props) {
     const { download, source, onLoad } = props;
 
-    // Call the on Load
+    const frameRef = React.useRef(null);
+
+
+    // The Current State
+    const [ count, setCount ] = React.useState(0);
+
+
+    // Call the Load
     React.useEffect(() => {
         if (download) {
+            window.setTimeout(onComplete, 100);
+        }
+    }, [ download ]);
+
+    // Test the Complete
+    const onComplete = () => {
+        if (!frameRef.current) {
+            return;
+        }
+        const frame    = frameRef.current;
+        const document = frame.contentDocument || frame.contentWindow.document;
+
+        if (document.readyState !== "complete") {
+            window.setTimeout(onComplete, 100);
+        } else {
             window.setTimeout(() => {
                 onLoad();
-            }, 1000);
+                setCount(count + 1);
+            }, 10000);
         }
-    }, [ download, source ]);
+    };
 
 
     // Do the Render
@@ -26,9 +49,10 @@ function Downloader(props) {
         return <React.Fragment />;
     }
     return <iframe
+        ref={frameRef}
         title="Downloader"
         name="downloader"
-        src={`${source}&rdm=${new Date().getTime()}`}
+        src={`${source}&rdm=${count}`}
         style={{ display : "none" }}
     />;
 }
