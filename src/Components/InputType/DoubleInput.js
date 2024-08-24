6c34464fd496e2408a02d28495f5f1988a7bd4bd
Tomas Malbran <malbrantomas@gmail.com>
2024-08-24 16:03:10 -0300

    Fix the Double Input styles when there is no Label

--- src/Components/InputType/DoubleInput.js
+++ src/Components/InputType/DoubleInput.js
@@ -12,24 +12,43 @@ import InputField           from "../Form/InputField";
 
 
 // Styles
-const Container = Styled.div`
+const Container = Styled.div.attrs(({ withLabel }) => ({ withLabel }))`
     width: 100%;
     display: flex;
     align-items: center;
-    gap: 16px;
 
-    .inputfield,
-    .inputfield input {
-        height: 20px;
-    }
     .inputfield:first-child::after {
         content: "";
         position: absolute;
-        top: calc(-2px - var(--input-label));
-        bottom: calc(-2px - var(--input-vert-padding));
-        right: -8px;
         border-right: 1px solid var(--input-border);
     }
+
+    ${(props) => props.withLabel ? `
+        gap: 16px;
+
+        .inputfield,
+        .inputfield input {
+            height: 20px;
+        }
+
+        .inputfield:first-child::after {
+            top: calc(-2px - var(--input-label));
+            bottom: calc(-2px - var(--input-vert-padding));
+            right: -8px;
+        }
+    ` : `
+        .input-content {
+            min-height: calc(var(--input-height) - 2px);
+            padding: var(--input-padding);
+            padding-top: var(--input-label) !important;
+        }
+
+        .inputfield:first-child::after {
+            top: 0;
+            bottom: 0;
+            right: 0;
+        }
+    `}
 `;
 
 
@@ -41,7 +60,7 @@ const Container = Styled.div`
  */
 function DoubleInput(props) {
     const {
-        className, isFocused, isDisabled,
+        className, isFocused, isDisabled, withLabel,
         onChange, onFocus, onBlur, children,
     } = props;
 
@@ -56,11 +75,11 @@ function DoubleInput(props) {
         className={className}
         isFocused={isFocused}
         isDisabled={isDisabled}
+        withLabel={withLabel}
+        withPadding={withLabel}
         withBorder
-        withPadding
-        withLabel
     >
-        <Container>
+        <Container withLabel={withLabel}>
             {items.map((item) => <InputField
                 {...item}
                 key={item.name}
@@ -85,6 +104,7 @@ DoubleInput.propTypes = {
     className  : PropTypes.string,
     isFocused  : PropTypes.bool,
     isDisabled : PropTypes.bool,
+    withLabel  : PropTypes.bool,
     onChange   : PropTypes.func.isRequired,
     onFocus    : PropTypes.func,
     onBlur     : PropTypes.func,
