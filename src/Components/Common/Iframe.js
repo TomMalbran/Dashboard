07f8bc20b0f1fff4c0d7c40cc5e79ec91a62b7bd
Tomas Malbran <malbrantomas@gmail.com>
2024-11-21 17:52:11 -0300

    Add an Iframe component

---
+++ src/Components/Common/Iframe.js
@@ -0,0 +1,73 @@
+import React                from "react";
+import PropTypes            from "prop-types";
+
+
+
+/**
+ * The Iframe Component
+ * @param {Object} props
+ * @returns {React.ReactElement}
+ */
+function Iframe(props) {
+    const { isHidden, className, content } = props;
+
+
+    // The Reference
+    const frameRef = React.useRef(null);
+
+    // The Current State
+    const [ height, setHeight ] = React.useState("0px");
+
+
+    // Updates the Height
+    const onLoad = () => {
+        const height = frameRef.current.contentWindow.document.body.scrollHeight;
+        console.log(height);
+        setHeight(`${height + 16}px`);
+    };
+
+    // Calls the on Load on Mount
+    React.useEffect(() => {
+        console.log("loaded");
+        onLoad();
+    }, []);
+
+
+
+    // Nothing to Render
+    if (isHidden || !content) {
+        return <React.Fragment />;
+    }
+
+    return <iframe
+        ref={frameRef}
+        className={className}
+        onLoad={onLoad}
+        srcDoc={content}
+        height={height}
+        // scrolling="no"
+        frameBorder="0"
+    />;
+}
+
+/**
+ * The Property Types
+ * @typedef {Object} propTypes
+ */
+Iframe.propTypes = {
+    isHidden  : PropTypes.bool,
+    className : PropTypes.string,
+    content   : PropTypes.string,
+};
+
+/**
+ * The Default Properties
+ * @type {Object} defaultProps
+ */
+Iframe.defaultProps = {
+    isHidden  : false,
+    className : "",
+    content   : "",
+};
+
+export default Iframe;
