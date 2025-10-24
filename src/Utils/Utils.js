b306054b46ff42547a175c7df67837abac9e829d
Tomas Malbran <malbrantomas@gmail.com>
2025-10-24 15:33:31 -0300

    Use a lower value for the contrast color

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1342,7 +1342,7 @@ function getContrastColor(value) {
         blue  = parseInt(value.substring(5, 7), 16);
     }
 
-    if ((red * 0.299) + (green * 0.587) + (blue * 0.114) > 186) {
+    if ((red * 0.299) + (green * 0.587) + (blue * 0.114) > 146) {
         result = "black";
     }
     return result;
